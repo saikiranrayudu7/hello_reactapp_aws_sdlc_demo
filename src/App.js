25b67acfbf034a32021fada0af54cc1e638d8c75
saikirun7 <saikirunrayudu@gmail.com>
2025-08-23 12:38:01 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>I completed AWS Certification.</h1>
-      <h1>I created multiple pipeline.</h1>
+      <h1>I completed AWS Certification</h1>
+      <h1>I created multiple pipeline</h1>
     </div>
   )
 }
