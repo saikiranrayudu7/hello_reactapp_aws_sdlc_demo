c5fb28d0545f6f50496d8553483bea602c21747c
saikirun7 <saikirunrayudu@gmail.com>
2025-08-23 13:03:49 +0530

    d

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>I completed AWS Certification</h1>
+      <h1>I completed AWS Certification.</h1>
       <h1>I created multiple pipeline</h1>
     </div>
   )
